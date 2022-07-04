b83fdd4148866e570fe17d1821e81770a551c461
David Telenko <davidtelenko11@gmail.com>
2022-07-04 15:56:14 +0300

    It works

--- include/my/format/join.hpp
+++ include/my/format/join.hpp
@@ -9,16 +9,40 @@
 
 namespace my {
 
+template <class T>
+concept pair_like = requires(T val) {
+    {val.first};
+    {val.second};
+};
+
+template <class T, class Ch, class Tr,
+          class R = std::remove_reference_t<T>>
+concept default_presentable =
+    std::ranges::range<R> or
+    my::is_tuple_v<R> or
+    pair_like<R> or
+    std::input_or_output_iterator<R> or
+    my::printable<R, std::basic_ostream<Ch, Tr>>;
+
+template <class Ch, class Tr, default_presentable<Ch, Tr> T>
+constexpr void present(std::basic_ostream<Ch, Tr>& os, const T& value);
+
+struct DefaultPresenter {
+    template <class Ch, class Tr, default_presentable<Ch, Tr> T>
+    constexpr void operator()(std::basic_ostream<Ch, Tr>& os,
+                              const T& value) const { present(os, value); }
+};
+
 template <std::input_iterator It, class Presenter>
 class JoinedRangeView {
    public:
     constexpr explicit JoinedRangeView(It first, It last,
                                        std::string_view delim,
-                                       Presenter present)
+                                       Presenter presenter)
         : _first(std::move(first)),
           _last(std::move(last)),
           _delim(std::move(delim)),
-          _present(std::move(present)) {
+          _present(std::move(presenter)) {
     }
 
     template <class Ch, class Tr>
@@ -53,10 +77,10 @@ class JoinedTupleView {
    public:
     constexpr explicit JoinedTupleView(const std::tuple<Args...>& tuple,
                                        std::string_view delim,
-                                       Presenter present)
+                                       Presenter presenter)
         : _tuple(tuple),
           _delim(std::move(delim)),
-          _present(std::move(present)) {
+          _present(std::move(presenter)) {
     }
 
     template <class Ch, class Tr>
@@ -85,30 +109,34 @@ class JoinedTupleView {
     my::make_member_function_t<Presenter> _present;
 };
 
-template <class T>
-concept pair_like = requires(T val) {
-    {val.first};
-    {val.second};
-};
-
+template <class Presenter = DefaultPresenter>
 struct PairPresenter {
+    constexpr explicit PairPresenter(std::string_view delim = ", ",
+                                     Presenter presenter = {})
+        : _delim(std::move(delim)), _present(presenter) {
+    }
+
     template <pair_like T, class Ch, class Tr>
     constexpr void operator()(std::basic_ostream<Ch, Tr>& os,
                               const T& value) const {
-        os << "("
-           << value.first
-           << ", "
-           << value.second
-           << ")";
+        _present(os, value.first);
+        os << _delim;
+        _present(os, value.second);
     }
+
+   private:
+    const std::string_view _delim;
+    Presenter _present;
 };
 
-constexpr PairPresenter pairPresent;
+constexpr PairPresenter<> pairPresent;
 
+template <class Presenter = DefaultPresenter>
 struct RangePresenter {
    public:
-    constexpr explicit RangePresenter(std::string_view delim = ", ")
-        : _delim(std::move(delim)) {
+    constexpr explicit RangePresenter(std::string_view delim = ", ",
+                                      Presenter presenter = {})
+        : _delim(std::move(delim)), _present(presenter) {
     }
 
     template <std::ranges::range Range, class Ch, class Tr>
@@ -120,23 +148,26 @@ struct RangePresenter {
         if (first == last) return;
 
         auto it = first;
-        os << *it;
+        _present(os, *it);
 
         for (++it; it != last; ++it) {
-            os << _delim << *it;
+            os << _delim;
+            _present(os, *it);
         }
     }
 
    private:
     const std::string_view _delim;
+    Presenter _present;
 };
 
-constexpr RangePresenter rangePresent;
-
+constexpr RangePresenter<> rangePresent;
+template <class Presenter = DefaultPresenter>
 struct TuplePresenter {
    public:
-    constexpr explicit TuplePresenter(std::string_view delim = ", ")
-        : _delim(std::move(delim)) {
+    constexpr explicit TuplePresenter(std::string_view delim = ", ",
+                                      Presenter presenter = {})
+        : _delim(std::move(delim)), _present(presenter) {
     }
 
     template <class Ch, class Tr, class... Args>
@@ -145,66 +176,53 @@ struct TuplePresenter {
         std::apply(
             [&os, this](const auto& arg,
                         const auto&... args) {
-                os << arg;
-                ((os << _delim << args), ...);
+                _present(os, arg);
+                ((os << _delim, _present(os, args)), ...);
             },
             value);
     }
 
    private:
     const std::string_view _delim;
+    Presenter _present;
 };
 
-constexpr TuplePresenter tuplePresent;
-
-template <class T, class Ch, class Tr,
-          class R = std::remove_reference_t<T>>
-concept default_presentable =
-    std::ranges::range<R> or
-    my::is_tuple_v<R> or
-    pair_like<R> or
-    std::input_or_output_iterator<R> or
-    my::printable<R, std::basic_ostream<Ch, Tr>>;
-
-// TODO reorder in favour of printable
-struct DefaultPresenter {
-    template <class Ch, class Tr, default_presentable<Ch, Tr> T>
-    constexpr void operator()(std::basic_ostream<Ch, Tr>& os,
-                              const T& value) const {
-        if constexpr (std::ranges::range<T>) {
-            rangePresent(os, value);
-        } else if constexpr (pair_like<T>) {
-            pairPresent(os, value);
-        } else if constexpr (my::is_tuple_v<T>) {
-            tuplePresent(os, value);
-        } else if constexpr (std::input_or_output_iterator<T>) {
-            os << *value;
-        } else {
-            os << value;
-        }
-    }
-};
-
-constexpr DefaultPresenter present;
+constexpr TuplePresenter<> tuplePresent;
 
 struct JoinFunction {
     template <std::ranges::range Range, class Presenter = DefaultPresenter>
     constexpr auto operator()(const Range& range,
                               std::string_view delim = ", ",
-                              Presenter present = {}) const {
+                              Presenter presenter = {}) const {
         return JoinedRangeView(std::ranges::begin(range),
                                std::ranges::end(range),
-                               delim, present);
+                               delim, presenter);
     }
 
     template <class Presenter, class... Args>
     constexpr auto operator()(const std::tuple<Args...>& tuple,
                               std::string_view delim = ", ",
-                              Presenter present = DefaultPresenter{}) const {
-        return JoinedTupleView(tuple, delim, present);
+                              Presenter presenter = DefaultPresenter{}) const {
+        return JoinedTupleView(tuple, delim, presenter);
     }
 };
 
 constexpr JoinFunction join;
+
+template <class Ch, class Tr, default_presentable<Ch, Tr> T>
+constexpr void present(std::basic_ostream<Ch, Tr>& os, const T& value) {
+    if constexpr (my::printable<T>) {
+        os << value;
+    } else if constexpr (std::ranges::range<T>) {
+        rangePresent(os, value);
+    } else if constexpr (pair_like<T>) {
+        pairPresent(os, value);
+    } else if constexpr (my::is_tuple_v<T>) {
+        tuplePresent(os, value);
+    } else if constexpr (std::input_or_output_iterator<T>) {
+        present(os, *value);
+    }
+    return;
+}
 
 }  // namespace my
