d8f69f87a7a9926ebecefc45ac4087310d71fe70
David Telen'ko <davidtelenko11@gmail.com>
2022-07-05 18:08:05 +0300

    Moved

---
+++ include/my/format/experimental/delimiters.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#ifndef MY_DELIMITERS_HPP
+#define MY_DELIMITERS_HPP
+
+#include <my/util/traits.hpp>
+
+namespace my {
+
+template <class T>
+struct delimiters {
+};
+
+template <my::iterable T>
+struct delimiters<T> {
+    constexpr static const char* open = "[";
+    constexpr static const char* delim = ", ";
+    constexpr static const char* close = "]";
+};
+
+template <my::iterator_concept T>
+struct delimiters<T> {
+    constexpr static const char* open = "[";
+    constexpr static const char* delim = ", ";
+    constexpr static const char* close = "]";
+};
+
+template <class... Ts>
+struct delimiters<std::tuple<Ts...>> {
+    constexpr static const char* open = "(";
+    constexpr static const char* delim = ", ";
+    constexpr static const char* close = ")";
+};
+
+template <class... Ts>
+struct delimiters<std::pair<Ts...>> {
+    constexpr static const char* open = "(";
+    constexpr static const char* delim = ", ";
+    constexpr static const char* close = ")";
+};
+
+}  // namespace my
+
+#endif  // MY_DELIMITERS_HPP
